78e4de0a986234e164c26d2d2ff1b8e5b17abe8c
Getabalewkemaw <getabalewkemaw@gmail.com>
2025-12-06 00:36:10 +0300

    implementing the hoem screen

--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -1,4 +1,4 @@
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require('nativewind/metro');
 const config = getDefaultConfig(__dirname)
-module.exports = withNativeWind(config, { input: './app/global.css' })
+module.exports = withNativeWind(config, { input: './app/globals.css' })
